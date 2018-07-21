a3cf4bf08fb6d2a737844b6ad8da4402f0ce33d3
Ezeoke Chukwunonso <ezeoke95@gmail.com>
2018-07-21 07:46:20 +0100

    Change database get rides query to get car details

--- dbServer/controllers/getRides.js
+++ dbServer/controllers/getRides.js
@@ -4,8 +4,10 @@ export default (req, res, next) => {
   const query = {
     text: `select concat_ws(
       ' ', firstname, lastname
-    ) as driver_name, rides.* from rides
-    inner join users on rides.user_id = users.id;`,
+    ) as driver_name, rides.*, cars.make as car_make,
+    cars.model as car_model from rides
+    inner join users on rides.user_id = users.id left outer join
+    cars on cars.id = users.car_id;`,
   };
   db.query(query, (error, response) => {
     if (error) return next(error);
