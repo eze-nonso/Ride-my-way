77caa441bfdcf2e7689ad10ba1fb34cf4ecdd4b4
Ezeoke Chukwunonso <ezeoke95@gmail.com>
2018-07-30 01:08:45 +0100

    Integrate input type checking in signup page

--- client/js-client/signup.js
+++ client/js-client/signup.js
@@ -1,5 +1,15 @@
 define(['./common'], (common) => {
   const signupForm = document.getElementById('js-signup');
+  const email = document.getElementById('email');
+  const password = document.getElementById('password');
+  const firstname = document.getElementById('firstname');
+  const lastname = document.getElementById('lastname');
+  const city = document.getElementById('city');
+  const state = document.getElementById('state');
+  const number = document.getElementById('number');
+  const carMake = document.getElementById('carmake');
+  const carModel = document.getElementById('carmodel');
+
   const route = '/api/v1/auth/signup';
 
   const processSignup = (evt) => {
@@ -15,6 +25,8 @@ define(['./common'], (common) => {
       lastname: signupForm.lastname.value,
       city: signupForm.city.value,
       state: signupForm.state.value,
+      carModel: signupForm.carmodel.value,
+      carMake: signupForm.carmake.value,
     };
 
     fetch(route, {
@@ -25,7 +37,54 @@ define(['./common'], (common) => {
       .then(res => Promise.all([res.json(), res]))
       .then(([data, res]) => {
         if (!res.ok) {
-          return alert(JSON.stringify(data));
+          const notifDiv = document.createElement('div');
+          notifDiv.setAttribute('id', 'notif-div');
+          notifDiv.style.setProperty('background', 'hsl(206, 21%, 94%)');
+          notifDiv.style.setProperty('color', '#ea0f0f');
+          notifDiv.style.setProperty('display', 'flex');
+          notifDiv.style.setProperty('font-size', '1.2em');
+          notifDiv.style.setProperty('flex-flow', 'row wrap');
+          notifDiv.style.setProperty('justify-content', 'center');
+          notifDiv.style.setProperty('font-weight', '600');
+          notifDiv.style.setProperty('border', '1px solid #c9c3c5');
+          notifDiv.style.setProperty('padding', '0 5px');
+
+          const notif = document.createElement('p');
+          notif.style.setProperty('margin', 'unset');
+          notif.style.setProperty('margin-top', 'auto');
+          notif.style.setProperty('margin-bottom', 'auto');
+
+          notif.textContent = data.message;
+          switch (true) {
+            case res.status === 409:
+              email.focus();
+              break;
+            case res.status === 422 && !!data.errors.email:
+              email.focus();
+              email.style.setProperty('background', '#fa2e2e73');
+              password.removeAttribute('style');
+              notif.textContent = 'Please enter a valid Email address';
+              break;
+            case res.status === 422 && !!data.errors.passwordType:
+              password.focus();
+              password.style.setProperty('background', '#fa2e2e73');
+              email.removeAttribute('style');
+              notif.textContent = 'Password should comprise letters and numbers and have min length of 8';
+              break;
+            default:
+              firstname.focus();
+              notif.textContent = 'Required fields cannot be empty strings';
+              email.removeAttribute('style');
+              password.removeAttribute('style');
+          }
+
+          notifDiv.innerHTML = '';
+          notifDiv.appendChild(notif);
+
+          const parentDiv = document.getElementById('one');
+          const oldNotif = parentDiv.querySelector('#notif-div');
+          if (oldNotif) parentDiv.removeChild(oldNotif);
+          return parentDiv.prepend(notifDiv);
         }
         common.allow();
         return common.store({
